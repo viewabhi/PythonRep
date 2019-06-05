3c2e08b50cb461d605bd24bb544176d4a03671bc
Sharma <abhishek.n.sharma@capgemini.com>
2019-06-05 18:41:07 +0530

    first commit

---
+++ student.py
@@ -0,0 +1,22 @@
+students = []
+
+
+class Student:
+
+    school_name = "DAV Public"
+
+    def __init__(self, name, student_id=123):
+        self.name = name
+        self.student_id = student_id
+        students.append(self)
+
+    def __str__(self):
+        return "Student " + self.name
+
+    def get_name_capitalize(self):
+        return self.name.capitalize()
+
+    def get_school_name(self):
+        return self.school_name
+
+
